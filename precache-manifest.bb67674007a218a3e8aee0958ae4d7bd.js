5314c063a4e68f2fecc62f691596719079eaa95f
anurag2541997 <a.dixit@iiitmanipur.ac.in>
2020-09-15 20:05:49 +0530

    Updates

---
+++ precache-manifest.bb67674007a218a3e8aee0958ae4d7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10398035db054f2de3478607f04ed1d6",
+    "url": "/Survey-Tiger/index.html"
+  },
+  {
+    "revision": "8270b0fa0c0efd1c10b1",
+    "url": "/Survey-Tiger/static/css/main.aa0f792b.chunk.css"
+  },
+  {
+    "revision": "f2f213fe5b4393ec30e8",
+    "url": "/Survey-Tiger/static/js/2.434592b1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Survey-Tiger/static/js/2.434592b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8270b0fa0c0efd1c10b1",
+    "url": "/Survey-Tiger/static/js/main.016be235.chunk.js"
+  },
+  {
+    "revision": "8b5b5c4aa27fcd1a59c8",
+    "url": "/Survey-Tiger/static/js/runtime-main.1474c825.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Survey-Tiger/static/media/logo.5d5d9eef.svg"
+  }
+]);
